0b79e94414d122f18f909a60240595727a65fd7f
Gelo <gelochiang2003@gmail.com>
2022-03-08 11:19:13 +0800

    Upload school practiceHW hw2.

---
+++ 1102_Object_Oriented_Programming/實習/Practice-2/Rectangle.h
@@ -0,0 +1,14 @@
+#ifndef _RECTANGLE_H_
+#define _RECTANGLE_H_
+
+class Rectangle
+{
+public:
+    double width;
+    double height;
+
+    double getArea();
+    double getPerimeter();
+};
+
+#endif
